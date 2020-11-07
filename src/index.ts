28bcbda384330d611443d0f0aad5c4e95aebfe2f
Haydon Lam <lamhaydon@gmail.com>
2020-11-07 16:56:12 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+  await page.goto("https:/www.google.com");
+  await page.screenshot({ path: "google.png" });
+
+  await browser.close();
+})();
