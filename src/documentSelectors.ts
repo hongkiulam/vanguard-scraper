f4a4e4ce8b4b9c2ffc8152f33945cf7e4f821ebe
Haydon Lam <haydonlam@pop-os.localdomain>
2020-11-15 19:45:50 +0000

    More verbose responses (error messges, success state)

--- src/documentSelectors.ts
+++ src/documentSelectors.ts
@@ -9,3 +9,6 @@ export const PERFORMANCEBUTTON = dataReactId(
 export const INVESTMENTSRETURNED = dataReactId(
   ".0.1:0:3.1.1:1.1:0.1:1.1.2.0.1:3.1:0.1.0.2.1.0"
 );
+export const USERNAMEINPUT = "#__GUID_1007";
+export const PASSWORDINPUT = "#__GUID_1008";
+export const LOGINBUTTON = dataReactId(".0.1:2.0.0.1.0.1:3.1.2.0");
--- src/index.ts
+++ src/index.ts
@@ -3,59 +3,104 @@ import express from "express";
 import {
   INVESTMENTSRETURNED,
   ISAVALUE,
+  LOGINBUTTON,
+  PASSWORDINPUT,
   PERFORMANCEBUTTON,
   RATEOFRETURN,
-} from "./dashboardSelectors";
+  USERNAMEINPUT,
+} from "./documentSelectors";
 require("dotenv").config();
 
 const app = express();
 app.listen(process.env.PORT || 3000);
 
+interface ResponseData {
+  success: boolean;
+  errorMsg?: string;
+  isaValue?: string | null;
+  rateOfReturn?: string | null;
+  investmentsReturned?: string | null;
+}
+
 const getTextContent = (el: Element) => {
   return el.textContent;
 };
-const main = async () => {
-  console.log("Launch Browser...");
-  const browser = await puppeteer.launch({});
-  console.log("-- Browser Launched");
-  const page = await browser.newPage();
-  console.log("Goto Vanguard Login...");
-  await page.goto("https://secure.vanguardinvestor.co.uk");
-  console.log("-- Reached Vanguard Page");
-  await page.focus("#__GUID_1007");
-  await page.keyboard.type(process.env.VANGUARD_USERNAME!);
-  await page.focus("#__GUID_1008");
-  await page.keyboard.type(process.env.VANGUARD_PASSWORD!);
-  await page.$eval(".submit button", (btn) =>
-    (btn as HTMLButtonElement).click()
-  );
-  console.log("Attempting Login...");
-  await page.waitForNavigation({ waitUntil: "networkidle2" });
-  await page.waitForSelector(ISAVALUE);
-  console.log("-- Reached Dashboard");
-  const isaValue = await page.$eval(ISAVALUE, getTextContent);
-  const rateOfReturn = await page.$eval(RATEOFRETURN, (el) => {
-    const val = el.textContent;
-    const isNegative = el.classList.contains("text-negative");
-    return isNegative ? "-" : "+" + val;
-  });
 
-  // goto performance page
-  await page.$eval(PERFORMANCEBUTTON, (btn) =>
-    (btn as HTMLAnchorElement).click()
-  );
-  await page.waitForSelector(INVESTMENTSRETURNED);
-  const investmentsReturned = await page.$eval(
-    INVESTMENTSRETURNED,
-    getTextContent
-  );
-  const data = { isaValue, rateOfReturn, investmentsReturned };
-  console.log(data);
-  await browser.close();
+const scrapeVanguard = async (username: string, password: string) => {
+  let data: ResponseData = { success: false };
+  data.errorMsg = "Failed to start session";
+  let browser;
+  try {
+    console.log("Launch Browser...");
+    browser = await puppeteer.launch({});
+    console.log("-- Browser Launched");
+    const page = await browser.newPage();
+    console.log("Goto Vanguard Login...");
+    await page.goto("https://secure.vanguardinvestor.co.uk");
+    console.log("-- Reached Vanguard Page");
+
+    // * LOGIN
+    await page.focus(USERNAMEINPUT);
+    await page.keyboard.type(username);
+    await page.focus(PASSWORDINPUT);
+    await page.keyboard.type(password);
+    await page.$eval(LOGINBUTTON, (btn) => (btn as HTMLButtonElement).click());
+    console.log("Attempting Login...");
+    const loginRequest = await page.waitForResponse(
+      "https://secure.vanguardinvestor.co.uk/en-GB/Api/Session/Login/Post"
+    );
+    const loginResponse: any = await loginRequest.json();
+    if (loginResponse?.Result.NavigateTo === null) {
+      console.log("-- Failed Login");
+      data.errorMsg = "Invalid username or password";
+      throw new Error();
+    }
+    // * NAVIGATE TO DASHBOARD
+    data.errorMsg = "Failed to load data";
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
+    await page.waitForSelector(ISAVALUE);
+    console.log("-- Reached Dashboard");
+    data.errorMsg = "Failed to get data";
+    const isaValue = await page.$eval(ISAVALUE, getTextContent);
+    const rateOfReturn = await page.$eval(RATEOFRETURN, (el) => {
+      const val = el.textContent;
+      const isNegative = el.classList.contains("text-negative");
+      return isNegative ? "-" : "+" + val;
+    });
+
+    // goto performance page
+    await page.$eval(PERFORMANCEBUTTON, (btn) =>
+      (btn as HTMLAnchorElement).click()
+    );
+    await page.waitForSelector(INVESTMENTSRETURNED);
+    const investmentsReturned = await page.$eval(
+      INVESTMENTSRETURNED,
+      getTextContent
+    );
+    data = { success: true, isaValue, rateOfReturn, investmentsReturned };
+    console.log(data);
+    await browser.close();
+  } catch {
+    await browser?.close();
+  }
   return data;
 };
 
 app.get("/", async (req, res) => {
-  const data = await main();
-  res.send(data);
+  let data: ResponseData = {
+    success: false,
+    errorMsg: "Missing username/ password query parameters",
+  };
+  const q = req.query;
+  if (
+    q.username &&
+    q.password &&
+    typeof q.username === "string" &&
+    typeof q.password === "string"
+  ) {
+    data = await scrapeVanguard(q.username, q.password);
+    res.send(data);
+  } else {
+    res.send(data);
+  }
 });
