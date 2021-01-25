dd89f5e078da04eb166c2dbf0aba1d96a5c0c46a
Haydon Lam <lamhaydon@gmail.com>
2021-01-25 21:21:57 +0000

    Fix API when used with docker

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -7,8 +7,10 @@ export const startSession = async (username: string, password: string) => {
   browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
   console.log("Goto Vanguard Login...");
-  await page.goto("https://secure.vanguardinvestor.co.uk");
-
+  await page.goto("https://secure.vanguardinvestor.co.uk", {
+    waitUntil: "networkidle2",
+    timeout: 60000, // wait for 1 minute before timing out
+  });
   await login(page, username, password);
   await page.waitForNavigation();
   const pageUrl = page.url();
