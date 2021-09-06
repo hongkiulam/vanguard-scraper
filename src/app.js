a69c22d0f6ba48586b97fae9551d6f2230ae3b99
Haydon Lam <lamhaydon@gmail.com>
2021-09-06 19:51:38 +0100

    Reference .env with path

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 const Vanguard = require("./vg");
 const http = require("http");
 const Router = require("router");
